db36989267ade43f4e0d8768e87ecb2a13ef0dca
james.lyons <james.lyons@dematic.com>
2023-06-26 22:11:07 +0100

    v1.0.11

--- src/db/mysqlConnection.ts
+++ src/db/mysqlConnection.ts
@@ -1,19 +1,28 @@
-//Service for Dematic Dashboard Screwfix trentham to collect data from WMS and push to DB - DB Connection
+//Service for Dematic Dashboard Screwfix trentham to collect data
 //Created by: JWL
 //Date: 2022-12-30
-//Last modified: 2023/03/05 19:08:19
+//Last modified: 2023/06/26 22:10:31
 //Version: 0.0.1
 
 import * as mysql from "mysql";
 //var mysql = require("mysql");
 
+const requiredEnvVariables = ["DB_HOST", "DB_USER", "DB_PASSWORD", "DB_DATABASE"];
+
+//get the env variables
+for (const envVar of requiredEnvVariables) {
+  if (!process.env[envVar]) {
+    console.log(`Missing required environment variable: ${envVar}`);
+    process.exit(1);
+  }
+}
+
 const pool = mysql.createPool({
-  connectionLimit: 5, //important
-  //host: "127.0.0.1",
-  host: "10.4.5.227",
-  user: "nodeUser",
-  password: "nodeuser",
-  database: "dematic_dashboard",
+  connectionLimit: 5,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   debug: false,
 });
 
