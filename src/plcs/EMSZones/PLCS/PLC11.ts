f1c25e6c60a4c08475d3faf7efa42ba2993e43ba
James <james.lyons@dematic.com>
2024-10-26 09:50:56 +0100

    PLC 11 IP Address Change

--- src/plcs/EMSZones/PLCS/PLC11.ts
+++ src/plcs/EMSZones/PLCS/PLC11.ts
@@ -1,7 +1,7 @@
 //Service for Dematic Dashboard Screwfix trentham to read ems Data from PLC 11
 //Created by: JWL
 //Date: 2023/02/03 03:38:36
-//Last modified: 2024/10/26 09:49:58
+//Last modified: 2024/10/26 09:50:46
 //Version: 0.0.1
 import snap7Types from "../../../misc/plc/types.js";
 import {
@@ -10,7 +10,7 @@ import {
 } from "../functions/updateDB.js";
 
 const plcConfig = {
-	ip: "10.4.2.31",
+	ip: "10.4.2.22",
 	rack: 0,
 	slot: 2,
 	name: "PLC13",
