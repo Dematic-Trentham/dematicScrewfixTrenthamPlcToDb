b621cae303078399e6dc6b38cfa733f7b33d2191
James <james.lyons@dematic.com>
2024-10-26 09:50:13 +0100

    PLC 11 Added

---
+++ src/plcs/EMSZones/PLCS/PLC11.ts
@@ -0,0 +1,76 @@
+//Service for Dematic Dashboard Screwfix trentham to read ems Data from PLC 11
+//Created by: JWL
+//Date: 2023/02/03 03:38:36
+//Last modified: 2024/10/26 09:49:58
+//Version: 0.0.1
+import snap7Types from "../../../misc/plc/types.js";
+import {
+	readAndInsertMultiple,
+	readAndInsertSingle,
+} from "../functions/updateDB.js";
+
+const plcConfig = {
+	ip: "10.4.2.31",
+	rack: 0,
+	slot: 2,
+	name: "PLC13",
+};
+
+export async function readEMSDataFromPLC11() {
+	const items = [
+		{
+			name: "PLC11_EEStopZone_Zone5",
+			location: "PLC11",
+			subLocation: "PLC11",
+			description: "EStop Monitoring Zone 4",
+			area: snap7Types.Area.S7AreaMK,
+			db: 0,
+			start: 70,
+			bit: 0,
+		},
+		{
+			name: "PLC11_EEStopZone_Zone11.1",
+			location: "PLC11",
+			subLocation: "PLC11",
+			description: "EStop Monitoring Zone 11.1 - First Floor",
+			area: snap7Types.Area.S7AreaMK,
+			db: 0,
+			start: 70,
+			bit: 1,
+		},
+		{
+			name: "PLC11_EEStopZone_Zone11.2",
+			location: "PLC11",
+			subLocation: "PLC11",
+			description: "EStop Monitoring Zone 11.2 - Second Floor",
+			area: snap7Types.Area.S7AreaMK,
+			db: 0,
+			start: 70,
+			bit: 2,
+		},
+		{
+			name: "PLC11_EEStopZone_Zone11.3",
+			location: "PLC11",
+			subLocation: "PLC11",
+			description: "EStop Monitoring Zone 11.3 - Third Floor",
+			area: snap7Types.Area.S7AreaMK,
+			db: 0,
+			start: 70,
+			bit: 3,
+		},
+		{
+			name: "PLC11_EEStopZone_Zone11.4",
+			location: "PLC11",
+			subLocation: "PLC11",
+			description: "EStop Monitoring Zone 11.4 - Fourth Floor",
+			area: snap7Types.Area.S7AreaMK,
+			db: 0,
+			start: 70,
+			bit: 4,
+		},
+	];
+
+	await readAndInsertMultiple(plcConfig, items);
+}
+
+export default { readEMSDataFromPLC11 };
--- src/plcs/EMSZones/masterEMS.ts
+++ src/plcs/EMSZones/masterEMS.ts
@@ -1,16 +1,18 @@
 //Service for Dematic Dashboard Screwfix trentham to read ems Data
 //Created by: JWL
 //Date: 2023/02/03 03:38:36
-//Last modified: 2024/10/02 06:24:24
+//Last modified: 2024/10/26 09:47:48
 //Version: 0.0.1
 //import plc33
 
+import PLC11 from "./PLCS/PLC11.js";
 import plc13 from "./PLCS/PLC13.js";
 import plc33 from "./PLCS/PLC33.js";
+
 async function checkAllEMS() {
 	console.log("Checking all EMS data");
 
-	//read data from plc33
+	await PLC11.readEMSDataFromPLC11();
 	await plc13.readEMSDataFromPLC13();
 	await plc33.readEMSDataFromPLC33();
 }
