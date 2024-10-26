8171d1aaba617690d0365066d08d0eb02bfaa1b6
James <james.lyons@dematic.com>
2024-10-26 10:09:32 +0100

    added plc 31 order start

--- src/plcs/EMSZones/masterEMS.ts
+++ src/plcs/EMSZones/masterEMS.ts
@@ -1,7 +1,7 @@
 //Service for Dematic Dashboard Screwfix trentham to read ems Data
 //Created by: JWL
 //Date: 2023/02/03 03:38:36
-//Last modified: 2024/10/26 10:05:55
+//Last modified: 2024/10/26 10:09:24
 //Version: 0.0.1
 //import plc33
 
@@ -13,6 +13,7 @@ import PLC21 from "./PLCS/PLC21.js";
 import PLC22 from "./PLCS/PLC22.js";
 import PLC23 from "./PLCS/PLC23.js";
 import PLC24 from "./PLCS/PLC24.js";
+import PLC31 from "./PLCS/PLC31.js";
 import PLC33 from "./PLCS/PLC33.js";
 
 async function checkAllEMS() {
@@ -25,6 +26,7 @@ async function checkAllEMS() {
 	await PLC22.readEMSDataFromPLC22();
 	await PLC23.readEMSDataFromPLC23();
 	await PLC24.readEMSDataFromPLC24();
+	await PLC31.readEMSDataFromPLC31();
 	await PLC33.readEMSDataFromPLC33();
 }
 
